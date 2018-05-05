b997a6fafb6b3f6a3ec1f4dd7ab58602772e0836
Thomas Sanlis <Thomas.sanlis@gmail.com>
2018-05-05 17:04:31 +0200

    :sparkles: Add header

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
   <div>
     <section class="header">
-      <categories :categories="categories"></categories>
-      <img class="logo" src="~assets/logo.svg">
-
-      <h2 class="subtitle">
-        WordPress as a Progressive Web App
-      </h2>
+      <!-- <categories :categories="categories"></categories> -->
+      <div class="header__logo">
+        <img class="logo" src="~assets/logo.png">
+      </div>
+      <div class="header__menu">
+        <button>Adhérer</button>
+        <img src="~assets/search-black.png" alt="">
+        <img src="~assets/menu-black.png" alt="">
+      </div>
     </section>
     <section class="container">
       <div>
@@ -19,9 +22,8 @@
 </template>
 
 <script>
-
 import { mapGetters } from 'vuex'
-import api from "../api/index";
+import api from '../api/index'
 import postList from '../components/postList.vue'
 import recentPosts from '../components/recentPosts.vue'
 import categories from '../components/categories.vue'
@@ -58,30 +60,70 @@ export default {
     }
   },
   computed: {
-    ...mapGetters([
-      'categories'
-    ])
+    ...mapGetters(['categories'])
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@font-face {
+  font-family: 'Raleway Bold';
+  src: url('~static/Raleway-Bold.ttf') format('truetype');
+}
 
 .header {
-  background-color:#1e5799;
-  background-image: linear-gradient(to right, #34495F, #3A5674);
+  height: 20vh;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #fbfbfb;
   text-align: center;
+  .header__logo {
+    height: 100%;
+    display: flex;
+    align-items: center;
+    margin: 0 4.5rem;
+    img {
+      width: auto;
+      height: 50%;
+    }
+  }
+  .header__menu {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 4.5rem;
+    button {
+      border: 2px solid #ec7c74;
+      background: #ec7c74;
+      border-radius: 44px;
+      font-size: 1rem;
+      font-family: 'Raleway Bold';
+      padding: 0.25rem 2rem;
+      color: #ffffff;
+      letter-spacing: 0;
+      text-align: center;
+      margin: 0 0.5rem;
+      cursor: pointer;
+    }
+    img {
+      height: 1rem;
+      margin: 0 0.5rem;
+      cursor: pointer;
+    }
+  }
 }
 .container {
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-  padding-top:60px;
+  padding-top: 60px;
 }
 
 .content {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   display: flex;
   justify-content: center;
   align-items: flex-start;
@@ -89,11 +131,12 @@ export default {
   margin: 0px auto;
   line-height: 150%;
   padding: 0px 30px 30px 30px;
-  max-width:1000px;
+  max-width: 1000px;
 }
 
 .title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
   /* 1 */
   display: block;
   font-weight: bold;
@@ -101,8 +144,8 @@ export default {
   color: #fff;
   letter-spacing: 1px;
   font-size: 52px;
-  padding:50px 5px 0px 5px;
-  text-align:center;
+  padding: 50px 5px 0px 5px;
+  text-align: center;
 }
 
 .subtitle {
@@ -110,8 +153,8 @@ export default {
   font-size: 28px;
   color: #fff;
   padding-bottom: 15px;
-  padding-left:5px;
-  text-align:center;
+  padding-left: 5px;
+  text-align: center;
 }
 
 .links {
@@ -119,25 +162,21 @@ export default {
 }
 
 /* Smartphones (portrait and landscape) ----------- */
-@media only screen 
-and (min-device-width : 320px) 
-and (max-device-width : 480px) {
-
+@media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
   .title {
     font-size: 26px;
-    line-height:36px;
-    padding:40px 10px 0px;
+    line-height: 36px;
+    padding: 40px 10px 0px;
   }
   .subtitle {
     font-size: 22px;
-    line-height:26px;
-    padding:5px 10px 15px;
+    line-height: 26px;
+    padding: 5px 10px 15px;
   }
   h3 {
     font-size: 34px;
     font-weight: 700;
     line-height: 44px;
   }
-
 }
 </style>
