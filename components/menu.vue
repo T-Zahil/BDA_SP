1a67a41bd4f804283c1e856ff71b85d9917bdc46
Thomas Sanlis <Thomas.sanlis@gmail.com>
2018-05-05 21:11:56 +0200

    :sparkles: Add menu

---
+++ components/menu.vue
@@ -0,0 +1,125 @@
+<template>
+  <div class='menu'>
+    <div class="menu__header">
+
+    </div>
+    <div class="menu__content">
+      <div class="content__bg-lines grid-1">
+        <div class="col-5"></div>
+        <div class="col-5"></div>
+        <div class="col-2"></div>
+      </div>
+      <div class="content__nav">
+        <div class="nav__block">
+          <h3>À propos</h3>
+          <a href="">Qu'est-ce que le BDA ?</a>
+          <a href="">L'équipe</a>
+          <a href="">Engagez-vous</a>
+          <a href="">Partenaires</a>
+        </div>
+        <div class="nav__block">
+          <h3>Nos activités</h3>
+          <a href="">Lorem Ipsum</a>
+          <a href="">L'équipe</a>
+          <a href="">Engagez-vous</a>
+          <a href="">Partenaires</a>
+        </div>
+        <div class="nav__block">
+          <h3>Les cours du BDA</h3>
+        </div>
+        <div class="nav__block">
+          <h3>À propos</h3>
+          <a href="">Qu'est-ce que le BDA ?</a>
+          <a href="">L'équipe</a>
+          <a href="">Engagez-vous</a>
+          <a href="">Partenaires</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'menu'
+}
+</script>
+
+<style lang="scss">
+.menu {
+  width: 35vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  right: 0;
+  top: 0;
+  background-color: #1f1f1f;
+  transition: all 0.2s ease;
+  .menu__header {
+    width: 100%;
+    height: 20vh;
+    background-color: #212222;
+  }
+  .menu__content {
+    width: 100%;
+    max-height: 80vh;
+    overflow: scroll;
+    position: relative;
+    padding-right: 4.5rem;
+    .content__bg-lines {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      margin: 0;
+      left: 0;
+      top: 0;
+      z-index: -1;
+      .col-5,
+      .col-2 {
+        border-left: 1px solid rgba(51, 51, 51, 0.4);
+      }
+    }
+    .content__nav {
+      display: flex;
+      flex-direction: column;
+      background-image: url('../assets/zigwigwi.png');
+      background-position-x: 90%;
+      background-size: auto 100%;
+      background-repeat: no-repeat;
+      .nav__block {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        padding-right: 3px;
+        margin: 0.75rem 0;
+        h3 {
+          font-family: 'Raleway Bold';
+          font-size: 1.1rem;
+          color: #ffffff;
+          margin: 1.15rem 0;
+        }
+        a {
+          height: 1.3rem;
+          display: flex;
+          align-items: center;
+          font-family: 'Raleway Regular';
+          font-size: 0.9rem;
+          color: #c9c9c9;
+          text-decoration: none;
+          margin: 0.15rem 0;
+          cursor: pointer;
+          transition: 0.2s all ease;
+          &:hover {
+            font-family: 'Raleway Bold';
+            font-size: 1.3rem;
+            color: #ec7c74;
+            text-shadow: -1.5rem 0px 1px #2f2f2f;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
+
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
+  <div class="wrapper">
+    <div class="menu-transition grid-1">
+      <div class="col-5"></div>
+      <div class="col-5"></div>
+      <div class="col-2"></div>
+    </div>
     <section class="header">
       <!-- <categories :categories="categories"></categories> -->
       <div class="header__logo">
@@ -7,8 +12,8 @@
       </div>
       <div class="header__menu">
         <button>Adhérer</button>
-        <img src="~assets/search-black.png" alt="">
-        <img src="~assets/menu-black.png" alt="">
+        <img v-if="!this.menu" src="~assets/menu-black.png" alt="" @click="animMenu()">
+        <img v-else src="~assets/close.png" alt="" @click="animMenu()" class="menu__above">
       </div>
     </section>
     <section class="container grid-1">
@@ -31,6 +36,9 @@
       <div class="bg col-4">
         <img src="~assets/home1.jpg" alt="">
       </div>
+      <transition name="fade">
+        <sidemenu v-if="this.menu"></sidemenu>
+      </transition>
       <!-- <post-list v-if="posts" :posts="posts" title="Recent Posts"></post-list> -->
     </section>
   </div>
@@ -42,11 +50,12 @@ import api from '../api/index'
 import postList from '../components/postList.vue'
 import recentPosts from '../components/recentPosts.vue'
 import categories from '../components/categories.vue'
+import sidemenu from '../components/menu.vue'
+import anime from 'animejs'
 
 export default {
-  components: { postList, categories, recentPosts },
+  components: { postList, categories, recentPosts, sidemenu },
   async asyncData({ params }) {
-    // We can use async/await ES6 feature
     let { data } = await api.getPosts()
 
     return {
@@ -66,7 +75,8 @@ export default {
   },
   data() {
     return {
-      title: 'default'
+      title: 'default',
+      menu: false
     }
   },
   mounted() {
@@ -76,6 +86,35 @@ export default {
   },
   computed: {
     ...mapGetters(['categories'])
+  },
+  methods: {
+    toggleMenu: function() {
+      this.menu = !this.menu
+    },
+    animMenu: function() {
+      var menuSlices = this.$el.querySelectorAll('.menu-transition div')
+      var instance = this
+      anime({
+        targets: menuSlices,
+        translateY: '-100%',
+        easing: 'linear',
+        duration: function(el, i, l) {
+          return 200 + i * 200
+        },
+        complete: function() {
+          instance.toggleMenu()
+          anime({
+            targets: menuSlices,
+            translateY: '100%',
+            easing: 'linear',
+            delay: 500,
+            duration: function(el, i, l) {
+              return 600 + i * 600
+            }
+          })
+        }
+      })
+    }
   }
 }
 </script>
@@ -93,6 +132,13 @@ export default {
 @font-face {
   font-family: 'PlayfairDisplay Bold';
   src: url('~static/PlayfairDisplay-Bold.ttf') format('truetype');
+}
+
+.wrapper {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
 }
 
 .header {
@@ -129,29 +175,35 @@ export default {
       text-align: center;
       margin: 0 0.5rem;
       cursor: pointer;
+      z-index: 1;
     }
     img {
-      height: 1rem;
-      margin-left: 0.5rem;
+      height: auto;
+      width: 1.5rem;
+      margin-left: 1rem;
       cursor: pointer;
+    }
+    .menu__above {
+      z-index: 1;
     }
   }
 }
 .container {
+  width: 100vw;
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   .bg-lines {
     width: 65%;
     height: 80vh;
     position: absolute;
-    left: 4rem;
+    left: 4.5rem;
     z-index: -1;
     .col-3 {
       border-left: 1px solid #eeeeee;
     }
   }
-  height: 80vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   .content {
     width: 65%;
     max-height: 80vh;
@@ -188,5 +240,26 @@ export default {
       mask-size: 100% calc(80vh - 2rem);
     }
   }
+}
+.menu-transition {
+  width: 35vw;
+  height: 100%;
+  position: absolute;
+  right: 0.5rem;
+  bottom: -100vh;
+  z-index: 10;
+  div {
+    background-color: black;
+    border-left: 1px solid rgba(51, 51, 51, 0.4);
+    z-index: 10;
+  }
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
